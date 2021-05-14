626a275aa7bf263c0b91b6176f0ac24852528c82
ascolm <mertascioglu@gmail.com>
2021-05-14 15:07:19 +0300

    style(datarow): spread out textfield attributes, add comments

--- src/components/Sidebar/DataTable/DataRow/DataRow.tsx
+++ src/components/Sidebar/DataTable/DataRow/DataRow.tsx
@@ -11,13 +11,49 @@ export interface Props {
 const DataRow: React.FC<Props> = ({ day, dayData, dataChangeHandler }) => {
   return (
     <TableRow>
-      <TableCell component="th">{day}</TableCell>
 
-      <TableCell align="left"><TextField variant='outlined' value={dayData.time} type='time' size='small' margin='none' inputProps={{style: {padding: '0.3rem'}}} onChange={(e) => dataChangeHandler(day, 'time', e.currentTarget.value)}/></TableCell>
+      {/* DAY */}
+      <TableCell component="th">
+        {day}
+      </TableCell>
 
-      <TableCell align="left"><TextField variant='outlined' value={dayData.location} size='small' margin='none' inputProps={{style: {padding: '0.3rem'}}} onChange={(e) => dataChangeHandler(day, 'location', e.currentTarget.value)}/></TableCell>
+      {/* TIME */}
+      <TableCell align="left">
+        <TextField
+          variant='outlined'
+          value={dayData.time}
+          type='time'
+          size='small'
+          margin='none'
+          inputProps={{style: {padding: '0.3rem'}}}
+          onChange={(e) => dataChangeHandler(day, 'time', e.currentTarget.value)}
+          />
+      </TableCell>
 
-      <TableCell align="left"><TextField variant='outlined' value={dayData.usage} size='small'margin='none' inputProps={{style: {padding: '0.3rem'}}} onChange={(e) => dataChangeHandler(day, 'usage', e.currentTarget.value)}/></TableCell>
+      {/* LOCATION */}
+      <TableCell align="left">
+        <TextField
+          variant='outlined'
+          value={dayData.location}
+          size='small'
+          margin='none'
+          inputProps={{style: {padding: '0.3rem'}}}
+          onChange={(e) => dataChangeHandler(day, 'location', e.currentTarget.value)}
+          />
+      </TableCell>
+
+      {/* USAGE */}
+      <TableCell align="left">
+        <TextField
+        variant='outlined'
+        value={dayData.usage}
+        size='small'
+        margin='none'
+        inputProps={{style: {padding: '0.3rem'}}}
+        onChange={(e) => dataChangeHandler(day, 'usage', e.currentTarget.value)}
+        />
+      </TableCell>
+
     </TableRow>
   );
 }
