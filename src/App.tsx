141e87842752778d78952fadaeb4260a3f46d247
ascolm <mertascioglu@gmail.com>
2021-05-14 11:06:36 +0300

    feat(theme.ts): create custom theme file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css';
+import projectTheme from 'theme';
+import { ThemeProvider } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme={projectTheme}>
+
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: '#0a064a'
+    },
+    secondary:{
+      main: '#2fece2'
+    }
+  }
+});
