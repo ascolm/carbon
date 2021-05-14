b2a70591a09efded0b91f9ff63b83c65a3aae152
ascolm <mertascioglu@gmail.com>
2021-05-14 11:15:34 +0300

    feat(app): add makeStyles for custom styling on app level

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css';
 import projectTheme from 'theme';
 import { ThemeProvider } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  }
+}));
+
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
+    <div className={classes.root}>
       <ThemeProvider theme={projectTheme}>
 
       </ThemeProvider>
