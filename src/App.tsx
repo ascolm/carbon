24b6c86127dca03edcca9eba30faa88e48e763e9
ascolm <mertascioglu@gmail.com>
2021-05-14 11:49:34 +0300

    refactor(app): separate style into its own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import './App.css';
 import projectTheme from 'theme';
 import { ThemeProvider } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  }
-}));
-
+import useStyles from './App-styles';
 
 function App() {
   const classes = useStyles();
